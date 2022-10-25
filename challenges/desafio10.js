4951f416e0724a317b73329e2d764a06b224bee8
skrowll <lucasdejesus.rocha@hotmail.com>
2022-10-25 19:17:46 -0300

    fix: req 7 and 10 lint error

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,3 @@
-
 db.produtos
   .find({ "valoresNutricionais.3.percentual": { $lte: 40, $gte: 30 } },
     { _id: 0, nome: 1 });
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,3 +1,3 @@
 db.produtos
-  .find({ $and: [{ vendidos: { $not: { $eq: 50 } } },{ tags: { $exists: false } }] },
+  .find({ $and: [{ vendidos: { $not: { $eq: 50 } } }, { tags: { $exists: false } }] },
     { _id: 0, nome: 1, vendidos: 1 });
