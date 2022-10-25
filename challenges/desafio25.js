25121d982b74b04a45dda57556511722bba8c82e
skrowll <lucasdejesus.rocha@hotmail.com>
2022-10-25 20:55:40 -0300

    fix(desafios): req 25 ok

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,7 +1,7 @@
 db.produtos
   .updateMany(
-    { "valoresNutricionais.2.percentual": { $gte: 40 } },
-    { $push: { tags: "muito sódio" } },
+    { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+    { $addToSet: { tags: "muito sódio" } },
   );
 db.produtos
   .find({}, { _id: 0, nome: 1, tags: 1 });
